cf74485e44fa818d63ca162fd0a2a9666d79f607
lsm0729 <lsm0729@bos-semi.com>
2025-05-09 11:32:10 +0900

    make venv

---
+++ make_venv.sh
@@ -0,0 +1,5 @@
+uv pip install -r ./requirements/requirements-convert_legacy_llama.txt
+uv pip install -r ./requirements/requirements-convert_hf_to_gguf.txt
+uv pip install -r ./requirements/requirements-tool_bench.txt
+uv pip install huggingface-hub==0.31.1
+uv pip install flask==3.1.0
